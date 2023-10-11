3e61119493168ee4f902e9eddb64f395b4101ac7
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-10-11 08:53:35 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -6,24 +6,21 @@ import {CartProvider} from './store/CardProvider';
 
 
 const App = () => {
-
   const [cartHandler, setCartHandler]=useState(false);
-
   const ShowCartHandler=()=>{
     setCartHandler(true);
   }
   const HideCartHandler=()=>{
     setCartHandler(false);
   }
-
   return (
-      <Fragment>
+      <CartProvider>
       {cartHandler && <Cart Onshowcart={HideCartHandler}/>}
       <Header onShowcart={ShowCartHandler}/>
       <main>
       <Meal/>
       </main>
-      </Fragment>
+      </CartProvider>
   );
 }
 
